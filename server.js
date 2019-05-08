523a8444040b98ad5acd321ed4f83aa8634f30d4
Timothy Shores <timothymshores@gmail.com>
2019-05-08 13:25:41 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+    res.send("Hello World!");
+});
+
+module.exports = server;
