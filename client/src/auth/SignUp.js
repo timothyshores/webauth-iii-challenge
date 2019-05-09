5c4ebe812f8cc8c04a9761832cc3b43eaa1dc298
Timothy Shores <timothymshores@gmail.com>
2019-05-09 15:44:32 -0500

    Create SignUp form in auth/SignUp.js

--- client/src/auth/SignUp.js
+++ client/src/auth/SignUp.js
@@ -4,13 +4,42 @@ import axios from 'axios';
 class SignUp extends Component {
     state = {
         username: '',
+        department: '',
         password: '',
     };
 
     render() {
         return (
             <>
-                <h2>SignUp</h2>
+                <h2>Sign Up</h2>
+                <form>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <label htmlFor="department">Department:</label>
+                    <input
+                        type="text"
+                        id="department"
+                        value={this.state.department}
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        onChange={this.handleChange}
+                        value={this.state.password}
+                    />
+                    <div>
+                        <button type="submit">Sign Up</button>
+                    </div>
+                </form>
             </>
         );
     }
