18e724fde1255160e9c17675e39de9a6864cf6b4
Timothy Shores <timothymshores@gmail.com>
2019-05-09 17:29:12 -0500

    Create an a href to log out in header of App.js and function to remove authorization from localStorage and redirect to "/"

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, withRouter } from 'react-router-dom';
 
 import Login from './auth/Login';
 import SignUp from './auth/SignUp';
@@ -7,23 +7,28 @@ import Users from './users/Users';
 
 import './App.css';
 
-function App() {
+function App(props) {
+    const logout = () => {
+        localStorage.removeItem('authorization');
+        window.location.pathname = '/'
+    }
+
     return (
         <div className="App">
             <header className="App-header">
                 <h1>JSON Web Token Client</h1>
-                <NavLink to="/">Home</NavLink>
-                &nbsp; | &nbsp;
-                <NavLink to="/login">Login</NavLink>
+                <NavLink to="/users">Users</NavLink>
                 &nbsp; | &nbsp;
                 <NavLink to="/signup">Sign Up</NavLink>
                 &nbsp; | &nbsp;
-                <NavLink to="/users">Users</NavLink>
+                <NavLink to="/login">Login</NavLink>
+                &nbsp; | &nbsp;
+                <a href="/" onClick={logout}>Logout</a>
             </header>
             <main>
-                <Route path="/login" component={Login} />
-                <Route path="/signup" component={SignUp} />
                 <Route path="/users" component={Users} />
+                <Route path="/signup" component={SignUp} />
+                <Route path="/login" component={Login} />
             </main>
         </div>
     );
